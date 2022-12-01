4c1f0fbaf0285a594872712d623633d3212d6bad
Estel CM <estel_927@hotmail.com>
2022-12-01 09:37:06 +0100

    forms js ampliado y get back to u message

--- javascript/formsandothers.js
+++ javascript/formsandothers.js
@@ -1,25 +1,38 @@
 
 function validateForm() {
 
-    let x = document.forms["form1"]["nameandsurname"].value;
-    if (x === "ironhack") {
+    let missingname = document.forms["form1"]["nameandsurname"].value;
+    if (missingname === "ironhack") {
         alert("You cannot be Ironhack, because I am Ironhack");
         return false;
     }
-    if (x === "") {
+    else if (missingname.length === 0) {
         alert("It must be filled with your name");
         return false;
     }
-    else {
-        return true;
+        //  else {
+    //   return true;
+    //  }
+     let missingat = document.forms["form1"]["email"].value;
+   if (missingat !== includes("@")) {
+        alert("Invalid email format please enter valid email id");
+      return false;
     }
-    // let missingat = document.forms["form1"]["email"].value;
-    // if (missingat !== includes("@")) {
-    //     alert("You are missing @ ");
-    //     return false;
-    // }
-    // else {
-    //     return true;
-    // }
+    //  else {
+    //   return true;
+    //  }
+let missingphone = document.forms["form1"]["phone"].value;
+if (typeof missingphone !=='number'){
+alert("Invalid phone format please enter valid characters");
+return false;
+}
+let missingmessage = document.forms["form1"]["message"].value;
+if (missingname.length === 0){
+alert("Don't you have any message for us? Please, fill the message gap and let us know!");
+return false;
+}
 
+else{
+    return true;
+}
 }
